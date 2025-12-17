ce0564b94d558412cb5f10abcdcc43c13eadb531
Dheenath Sundararajan <rohitkumarsundararajan@gmail.com>
2025-12-17 21:42:16 +0530

    initial UserRepository with JPA query methods v1

---
+++ src/main/java/com/ecom/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.ecom.repository;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.ecom.entity.User;
+
+public class UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);    
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.username = :username OR u.email = :email")
+    Optional<User> findByUsernameOrEmail(@Param("username") String username, @Param("email")String email);
+}
